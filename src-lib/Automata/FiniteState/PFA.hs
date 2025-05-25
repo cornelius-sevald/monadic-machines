b5457289c5dfa7bf74ec0823afbcee0395103444
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2025-05-25 15:36:07 +0200

    Change acceptance function for PFA
    Make it match the definition from the report better.

--- src-lib/Automata/FiniteState/PFA.hs
+++ src-lib/Automata/FiniteState/PFA.hs
@@ -13,16 +13,25 @@ import qualified Numeric.Matrix as M
 import Numeric.Probability.Distribution (T, (??))
 
 data PFA prob a s = PFA
-  { start :: s,
+  { -- The start state q_1.
+    start :: s,
+    -- The set of final states F.
     final :: Set s,
+    -- The transition function δ.
     trans :: (s, a) -> T prob s
   }
 
-step :: (Eq s, Finite s, MatrixElement prob) => PFA prob a s -> a -> Matrix prob
+-- The transition matrix A(x).
+step ::
+  (Eq s, Finite s, MatrixElement prob) =>
+  PFA prob a s ->
+  a ->
+  Matrix prob
 step pfa x = M.fromList [[p j i x | i <- universeF] | j <- universeF]
   where
     p j i y = (== i) ?? trans pfa (j, y)
 
+-- The global transition matrix A*(w).
 steps ::
   forall a s prob.
   (Eq s, Finite s, MatrixElement prob) =>
@@ -36,14 +45,20 @@ steps pfa w =
     n = fromIntegral $ unTagged (cardinality :: Tagged s Natural)
 
 accepts ::
+  forall a s prob.
   (Eq s, Enum s, Finite s, Ord prob, MatrixElement prob) =>
   PFA prob a s ->
   prob ->
   [a] ->
   Bool
 accepts pfa η w =
-  let i1 = (succ . fromEnum) $ start pfa
-      js = Set.map (succ . fromEnum) $ final pfa
-      mat = steps pfa w
-      fs = M.select (\(i, j) -> i == i1 && j `Set.member` js) mat
-   in sum fs > η
+  let mat = steps pfa w
+   in M.at (v * mat * f) (1, 1) > η
+  where
+    v = M.matrix (1, n) (\(_, j) -> ind (j == j0))
+    f = M.matrix (n, 1) (\(i, _) -> ind (i `Set.member` is))
+    n = fromIntegral $ unTagged (cardinality :: Tagged s Natural)
+    j0 = (succ . fromEnum) $ start pfa
+    is = Set.map (succ . fromEnum) $ final pfa
+    ind :: Bool -> prob
+    ind b = if b then 1 else 0
